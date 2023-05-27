73ac224bbc967381acc991541db1008b4586f137
Marty <vsevolodmartynyuk@yandex.ru>
2023-05-27 13:39:33 +0700

    В тестовый клиент добавлена возможность параллельно отправлять запросы на несколько датчиков

--- Portfolio/client.py
+++ Portfolio/client.py
@@ -1,8 +1,26 @@
 import socket
+from threading import Thread
 
 udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_client.sendto(b"Hello!", ("127.0.0.1", 2222))
+udp_client.sendto(b"Hello! i'm 1111", ("127.0.0.1", 1111))
+udp_client.sendto(b"Hello! i'm 2222", ("127.0.0.1", 2222))
 
-request, address = udp_client.recvfrom(1024)
-udp_client.sendto(b"ping", address)
-print(request, *address)
+
+def func1(recvfrom):
+    request, address = recvfrom
+    udp_client.sendto(b"pong", address)
+    print(request, *address)
+
+
+def func2(recvfrom):
+    request, address = recvfrom
+    udp_client.sendto(b"pong", address)
+    print(request, *address)
+
+
+Thread(target=func1(udp_client.recvfrom(1024))).start()
+Thread(target=func2(udp_client.recvfrom(1024))).start()
+
+# request, address = udp_client.recvfrom(1024)
+# udp_client.sendto(b"ping", address)
+
