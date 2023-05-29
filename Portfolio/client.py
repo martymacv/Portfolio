d05e4ddd7403a9b034215300aba0c92419338444
martymacv <119780284+martymacv@users.noreply.github.com>
2023-05-29 09:18:41 +0700

    Martymacv (#1)
    * Добавил второй экземляр Датчика
    
    * Сенсор отвечает на запрос клиента и отправляет ему байт-пакет
    
    * Регистр формирует список точек, по которым можно задать форму сигнала
    
    * Клиент может работать в многопоточном режиме. Каждый экземпляр клиента может запрашивать данные регистров только у своего сенсора

--- Portfolio/client.py
+++ Portfolio/client.py
@@ -1,25 +1,21 @@
 import socket
 from threading import Thread
 
-udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_client.sendto(b"Hello! i'm 1111", ("127.0.0.1", 1111))
-udp_client.sendto(b"Hello! i'm 2222", ("127.0.0.1", 2222))
+
+udp_clients = [[socket.socket(socket.AF_INET, socket.SOCK_DGRAM), i] for i in range(2222, 2223)]
 
 
-def func1(recvfrom):
-    request, address = recvfrom
-    udp_client.sendto(b"pong", address)
+def func1(udp_client_):
+    udp_client_[0].settimeout(100)
+    udp_client_[0].sendto(b"Hello! i'm 1111", ("127.0.0.1", udp_client_[1]))
+    request, address = udp_client_[0].recvfrom(1024)
+    # udp_client.sendto(b"pong", address)
     print(request, *address)
 
 
-def func2(recvfrom):
-    request, address = recvfrom
-    udp_client.sendto(b"pong", address)
-    print(request, *address)
-
-
-Thread(target=func1(udp_client.recvfrom(1024))).start()
-Thread(target=func2(udp_client.recvfrom(1024))).start()
+for udp_client in udp_clients:
+    Thread(target=func1(udp_client)).start()
+# Thread(target=func2(udp_client.recvfrom(1024))).start()
 
 # request, address = udp_client.recvfrom(1024)
 # udp_client.sendto(b"ping", address)
--- Portfolio/sensors.py
+++ Portfolio/sensors.py
@@ -1,7 +1,9 @@
+import asyncio
 import random
 import socket
 import json
 from threading import Thread
+from registers import Register
 
 """Итак, я хочу создавать объекты типа "Датчик", который может:
     1. обмениваться данными с агентом (клентом или другим сервером)
@@ -13,40 +15,82 @@ from threading import Thread
 
 
 class Sensor:
-    def __init__(self, type_server, sensor_id, ip, port):
+    def __init__(self, type_server, sensor_id, ip, port, registers):
         self.__type_server = type_server
         self.__sensor_id = sensor_id
         self.__ip = ip
         self.__port = port
+        self.registers = registers
+        self.__len_registers = len(self.registers)
         if self.__type_server == "UDP_SERVER":
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         elif self.__type_server == "TCP_SERVER":
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         else:
             pass
+        self.__save_config(self.__type_server, self.__sensor_id, self.__ip, self.__port)
 
     @classmethod
     def __save_config(cls, type_server, sensor_id, ip, port):
         with open(f"sensors_log/sensor_{sensor_id[-4:]}_config.json", "w") as f:
             json.dump({"type_server": type_server, "sensor_id": sensor_id, "ip": ip, "port": port}, f, indent=4)
 
+    # @classmethod
+    # def __save_signals(cls, sensor_id, signals):
+    #     with open(f"sensors_log/sensor_{sensor_id[-4:]}_config.json", "r+") as f:
+    #         # f.write("\n\n")
+    #         json.dump({"register_id": signals}, f, indent=4)
+
     def run(self):
-        self.__save_config(self.__type_server, self.__sensor_id, self.__ip, self.__port)
+
+        # self.__save_signals(self.__sensor_id, self.registers)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((self.__ip, self.__port))
         print(f"{self.__sensor_id} is run...")
+        i = 0
+        output = [b'' for _ in range(self.__len_registers)]
         while True:
             request, address = self.sock.recvfrom(1024)
-            self.sock.sendto(b"ping", address)
+            for reg in self.registers:
+                output[reg.unit_id - 1] = reg.value[i % reg.value[~0]]
+            self.sock.sendto(b''.join(output), address)
             print(request, *address)
+            i += 1
             # return request, *address
+
+    async def start(self):
+        await asyncio.gather(*self.__registers)
+
+
+async def start(regs):
+    await asyncio.gather(*[regs[i].run() for i in range(10000)])
 
 
 if __name__ == "__main__":
     sens_id = "TEMPERATURE_SENSOR_" + str(777).rjust(4, "0")
-    sensor_1 = Sensor("UDP_SERVER", sens_id, "localhost", 2222)
-    sens_id = "TEMPERATURE_SENSOR_" + str(999).rjust(4, "0")
-    sensor_2 = Sensor("UDP_SERVER", sens_id, "localhost", 1111)
-    Thread(target=sensor_1.run).start()
-    Thread(target=sensor_2.run).start()
+    sensor_1 = Sensor("UDP_SERVER", sens_id, "", 2222, [Register(1, (-25000, 25000), "R", 1),
+                                                        Register(10, (-2500, 2500), "R", 2),
+                                                        Register(100, (0, 25000), "R", 3)])
+    sensor_1.run()
+    # i = 0
+    # output = [0, 0, 0]
+    # while True:
+    #     for register in sensor_1.registers:
+    #         output[register.unit_id - 1] = register.value[i % register.value[~0]]
+    #     print(output)
+    #     i += 1
+    # создаем объект Sensor передаем в него объекты Register и запускаем последние асинхронно
+    # asyncio.run(start(registers))
+    # for i in registers:
+    #     asyncio.run(i.run())
+    # for register in registers:
+    #     Thread(target=register.run).start()
+
+    # sens_id = "TEMPERATURE_SENSOR_" + str(777).rjust(4, "0")
+    # sensor = Sensor("UDP_SERVER", sens_id, "localhost", 2222)
+    # abc = sensor.getvalue[0]
+    # sens_id = "TEMPERATURE_SENSOR_" + str(999).rjust(4, "0")
+    # sensor_2 = Sensor("UDP_SERVER", sens_id, "localhost", 1111)
+    # Thread(target=sensor_1.run).start()
+    # Thread(target=sensor_2.run).start()
 
